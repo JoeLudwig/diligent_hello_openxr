f38bcb4000383fab72ac8cd7d02205076616fdca
Joe Ludwig <joe@valvesoftware.com>
2021-07-13 20:12:49 -0700

    Switch the helloXr app to be based on the cube example
    It actually uses a projection matrix and a depth buffer, which will be pretty important for stereo rendering.

--- src/helloxr.cpp
+++ src/helloxr.cpp
@@ -66,12 +66,16 @@
 #include "Graphics/GraphicsEngineD3D12/interface/EngineFactoryD3D12.h"
 #include "Graphics/GraphicsEngineOpenGL/interface/EngineFactoryOpenGL.h"
 #include "Graphics/GraphicsEngineVulkan/interface/EngineFactoryVk.h"
+#include "Common/interface/BasicMath.hpp"
+#include "Common/interface/Timer.hpp"
 
 #include "Graphics/GraphicsEngine/interface/RenderDevice.h"
 #include "Graphics/GraphicsEngine/interface/DeviceContext.h"
 #include "Graphics/GraphicsEngine/interface/SwapChain.h"
 
 #include "Common/interface/RefCntAutoPtr.hpp"
+
+#include "Graphics/GraphicsTools/interface/MapHelper.hpp"
 
 #include "openxr/openxr.h"
 
@@ -266,6 +270,7 @@ public:
                 auto* GetEngineFactoryD3D11 = LoadGraphicsEngineD3D11();
 #    endif
                 auto* pFactoryD3D11 = GetEngineFactoryD3D11();
+                m_pEngineFactory = pFactoryD3D11;
                 pFactoryD3D11->CreateDeviceAndContextsD3D11(EngineCI, &m_pDevice, &m_pImmediateContext);
                 Win32NativeWindow Window{hWnd};
                 pFactoryD3D11->CreateSwapChainD3D11(m_pDevice, m_pImmediateContext, SCDesc, FullScreenModeDesc{}, Window, &m_pSwapChain);
@@ -294,6 +299,7 @@ public:
 				EngineCI.GraphicsAPIVersion = Version { 12, 0 };
 
                 auto* pFactoryD3D12 = GetEngineFactoryD3D12();
+				m_pEngineFactory = pFactoryD3D12;
                 pFactoryD3D12->CreateDeviceAndContextsD3D12(EngineCI, &m_pDevice, &m_pImmediateContext);
                 Win32NativeWindow Window{hWnd};
                 pFactoryD3D12->CreateSwapChainD3D12(m_pDevice, m_pImmediateContext, SCDesc, FullScreenModeDesc{}, Window, &m_pSwapChain);
@@ -319,6 +325,7 @@ public:
                 auto GetEngineFactoryOpenGL = LoadGraphicsEngineOpenGL();
 #    endif
                 auto* pFactoryOpenGL = GetEngineFactoryOpenGL();
+				m_pEngineFactory = pFactoryOpenGL;
 
                 EngineGLCreateInfo EngineCI;
                 EngineCI.Window.hWnd = hWnd;
@@ -341,6 +348,7 @@ public:
                 EngineVkCreateInfo EngineCI;
 
                 auto* pFactoryVk = GetEngineFactoryVk();
+				m_pEngineFactory = pFactoryVk;
                 pFactoryVk->CreateDeviceAndContextsVk(EngineCI, &m_pDevice, &m_pImmediateContext);
 
                 if (!m_pSwapChain && hWnd != nullptr)
@@ -361,6 +369,10 @@ public:
 
         if ( !CreateSession() )
             return false;
+
+        CreatePipelineState();
+        CreateVertexBuffer();
+        CreateIndexBuffer();
 
         return true;
     }
@@ -702,90 +714,8 @@ public:
 	IRenderDeviceD3D12* GetD3D12Device() { return (IRenderDeviceD3D12*)m_pDevice.RawPtr(); }
 #endif
 
-    void CreateResources()
-    {
-        // Pipeline state object encompasses configuration of all GPU stages
-
-        GraphicsPipelineStateCreateInfo PSOCreateInfo;
-
-        // Pipeline state name is used by the engine to report issues.
-        // It is always a good idea to give objects descriptive names.
-        PSOCreateInfo.PSODesc.Name = "Simple triangle PSO";
-
-        // This is a graphics pipeline
-        PSOCreateInfo.PSODesc.PipelineType = PIPELINE_TYPE_GRAPHICS;
-
-        // clang-format off
-        // This tutorial will render to a single render target
-        PSOCreateInfo.GraphicsPipeline.NumRenderTargets             = 1;
-        // Set render target format which is the format of the swap chain's color buffer
-        PSOCreateInfo.GraphicsPipeline.RTVFormats[0]                = m_pSwapChain->GetDesc().ColorBufferFormat;
-        // Use the depth buffer format from the swap chain
-        PSOCreateInfo.GraphicsPipeline.DSVFormat                    = m_pSwapChain->GetDesc().DepthBufferFormat;
-        // Primitive topology defines what kind of primitives will be rendered by this pipeline state
-        PSOCreateInfo.GraphicsPipeline.PrimitiveTopology            = PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-        // No back face culling for this tutorial
-        PSOCreateInfo.GraphicsPipeline.RasterizerDesc.CullMode      = CULL_MODE_NONE;
-        // Disable depth testing
-        PSOCreateInfo.GraphicsPipeline.DepthStencilDesc.DepthEnable = False;
-        // clang-format on
-
-        ShaderCreateInfo ShaderCI;
-        // Tell the system that the shader source code is in HLSL.
-        // For OpenGL, the engine will convert this into GLSL under the hood
-        ShaderCI.SourceLanguage = SHADER_SOURCE_LANGUAGE_HLSL;
-        // OpenGL backend requires emulated combined HLSL texture samplers (g_Texture + g_Texture_sampler combination)
-        ShaderCI.UseCombinedTextureSamplers = true;
-        // Create a vertex shader
-        RefCntAutoPtr<IShader> pVS;
-        {
-            ShaderCI.Desc.ShaderType = SHADER_TYPE_VERTEX;
-            ShaderCI.EntryPoint      = "main";
-            ShaderCI.Desc.Name       = "Triangle vertex shader";
-            ShaderCI.Source          = VSSource;
-            m_pDevice->CreateShader(ShaderCI, &pVS);
-        }
-
-        // Create a pixel shader
-        RefCntAutoPtr<IShader> pPS;
-        {
-            ShaderCI.Desc.ShaderType = SHADER_TYPE_PIXEL;
-            ShaderCI.EntryPoint      = "main";
-            ShaderCI.Desc.Name       = "Triangle pixel shader";
-            ShaderCI.Source          = PSSource;
-            m_pDevice->CreateShader(ShaderCI, &pPS);
-        }
-
-        // Finally, create the pipeline state
-        PSOCreateInfo.pVS = pVS;
-        PSOCreateInfo.pPS = pPS;
-        m_pDevice->CreateGraphicsPipelineState(PSOCreateInfo, &m_pPSO);
-    }
-
-    void Render()
-    {
-        // Set render targets before issuing any draw command.
-        // Note that Present() unbinds the back buffer if it is set as render target.
-        auto* pRTV = m_pSwapChain->GetCurrentBackBufferRTV();
-        auto* pDSV = m_pSwapChain->GetDepthBufferDSV();
-        m_pImmediateContext->SetRenderTargets(1, &pRTV, pDSV, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
-
-        // Clear the back buffer
-        const float ClearColor[] = {0.350f, 0.350f, 0.350f, 1.0f};
-        // Let the engine perform required state transitions
-        m_pImmediateContext->ClearRenderTarget(pRTV, ClearColor, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
-        m_pImmediateContext->ClearDepthStencil(pDSV, CLEAR_DEPTH_FLAG, 1.f, 0, RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
-
-        // Set the pipeline state in the immediate context
-        m_pImmediateContext->SetPipelineState(m_pPSO);
-
-        // Typically we should now call CommitShaderResources(), however shaders in this example don't
-        // use any resources.
-
-        DrawAttribs drawAttrs;
-        drawAttrs.NumVertices = 3; // Render 3 vertices
-        m_pImmediateContext->Draw(drawAttrs);
-    }
+    void Render();
+    void Update( double currTime, double elapsedTime );
 
     void Present()
     {
@@ -800,12 +730,23 @@ public:
 
     RENDER_DEVICE_TYPE GetDeviceType() const { return m_DeviceType; }
 
+	void CreatePipelineState();
+	void CreateVertexBuffer();
+	void CreateIndexBuffer();
+
 private:
+	RefCntAutoPtr<IPipelineState>         m_pPSO;
+	RefCntAutoPtr<IShaderResourceBinding> m_pSRB;
+	RefCntAutoPtr<IBuffer>                m_CubeVertexBuffer;
+	RefCntAutoPtr<IBuffer>                m_CubeIndexBuffer;
+	RefCntAutoPtr<IBuffer>                m_VSConstants;
+	float4x4                              m_WorldViewProjMatrix;
+	RefCntAutoPtr<IEngineFactory>         m_pEngineFactory;
+
     RefCntAutoPtr<IRenderDevice>  m_pDevice;
     RefCntAutoPtr<IDeviceContext> m_pImmediateContext;
     RefCntAutoPtr<ISwapChain>     m_pSwapChain;
 	std::vector< RefCntAutoPtr<ITextureView> >  m_rpEyeSwapchainViews[2];
-    RefCntAutoPtr<IPipelineState> m_pPSO;
     RENDER_DEVICE_TYPE            m_DeviceType = RENDER_DEVICE_TYPE_D3D11;
 
     XrInstance m_instance;
@@ -814,6 +755,241 @@ private:
     XrViewConfigurationView m_views[ 2 ] = {};
     XrSwapchain m_swapchain = XR_NULL_HANDLE;
 };
+
+void HelloXrApp::CreatePipelineState()
+{
+	// Pipeline state object encompasses configuration of all GPU stages
+
+	GraphicsPipelineStateCreateInfo PSOCreateInfo;
+
+	// Pipeline state name is used by the engine to report issues.
+	// It is always a good idea to give objects descriptive names.
+	PSOCreateInfo.PSODesc.Name = "Cube PSO";
+
+	// This is a graphics pipeline
+	PSOCreateInfo.PSODesc.PipelineType = PIPELINE_TYPE_GRAPHICS;
+
+	// clang-format off
+	// This tutorial will render to a single render target
+	PSOCreateInfo.GraphicsPipeline.NumRenderTargets = 1;
+	// Set render target format which is the format of the swap chain's color buffer
+	PSOCreateInfo.GraphicsPipeline.RTVFormats[ 0 ] = m_pSwapChain->GetDesc().ColorBufferFormat;
+	// Set depth buffer format which is the format of the swap chain's back buffer
+	PSOCreateInfo.GraphicsPipeline.DSVFormat = m_pSwapChain->GetDesc().DepthBufferFormat;
+	// Primitive topology defines what kind of primitives will be rendered by this pipeline state
+	PSOCreateInfo.GraphicsPipeline.PrimitiveTopology = PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+	// Cull back faces
+	PSOCreateInfo.GraphicsPipeline.RasterizerDesc.CullMode = CULL_MODE_BACK;
+	// Enable depth testing
+	PSOCreateInfo.GraphicsPipeline.DepthStencilDesc.DepthEnable = True;
+	// clang-format on
+
+	ShaderCreateInfo ShaderCI;
+	// Tell the system that the shader source code is in HLSL.
+	// For OpenGL, the engine will convert this into GLSL under the hood.
+	ShaderCI.SourceLanguage = SHADER_SOURCE_LANGUAGE_HLSL;
+
+	// OpenGL backend requires emulated combined HLSL texture samplers (g_Texture + g_Texture_sampler combination)
+	ShaderCI.UseCombinedTextureSamplers = true;
+
+	// In this tutorial, we will load shaders from file. To be able to do that,
+	// we need to create a shader source stream factory
+	RefCntAutoPtr<IShaderSourceInputStreamFactory> pShaderSourceFactory;
+	m_pEngineFactory->CreateDefaultShaderSourceStreamFactory( nullptr, &pShaderSourceFactory );
+	ShaderCI.pShaderSourceStreamFactory = pShaderSourceFactory;
+	// Create a vertex shader
+	RefCntAutoPtr<IShader> pVS;
+	{
+		ShaderCI.Desc.ShaderType = SHADER_TYPE_VERTEX;
+		ShaderCI.EntryPoint = "main";
+		ShaderCI.Desc.Name = "Cube VS";
+		ShaderCI.FilePath = "cube.vsh";
+		m_pDevice->CreateShader( ShaderCI, &pVS );
+		// Create dynamic uniform buffer that will store our transformation matrix
+		// Dynamic buffers can be frequently updated by the CPU
+		BufferDesc CBDesc;
+		CBDesc.Name = "VS constants CB";
+		CBDesc.uiSizeInBytes = sizeof( float4x4 );
+		CBDesc.Usage = USAGE_DYNAMIC;
+		CBDesc.BindFlags = BIND_UNIFORM_BUFFER;
+		CBDesc.CPUAccessFlags = CPU_ACCESS_WRITE;
+		m_pDevice->CreateBuffer( CBDesc, nullptr, &m_VSConstants );
+	}
+
+	// Create a pixel shader
+	RefCntAutoPtr<IShader> pPS;
+	{
+		ShaderCI.Desc.ShaderType = SHADER_TYPE_PIXEL;
+		ShaderCI.EntryPoint = "main";
+		ShaderCI.Desc.Name = "Cube PS";
+		ShaderCI.FilePath = "cube.psh";
+		m_pDevice->CreateShader( ShaderCI, &pPS );
+	}
+
+	// clang-format off
+	// Define vertex shader input layout
+	LayoutElement LayoutElems[] =
+	{
+		// Attribute 0 - vertex position
+		LayoutElement{0, 0, 3, VT_FLOAT32, False},
+		// Attribute 1 - vertex color
+		LayoutElement{1, 0, 4, VT_FLOAT32, False}
+	};
+	// clang-format on
+	PSOCreateInfo.GraphicsPipeline.InputLayout.LayoutElements = LayoutElems;
+	PSOCreateInfo.GraphicsPipeline.InputLayout.NumElements = _countof( LayoutElems );
+
+	PSOCreateInfo.pVS = pVS;
+	PSOCreateInfo.pPS = pPS;
+
+	// Define variable type that will be used by default
+	PSOCreateInfo.PSODesc.ResourceLayout.DefaultVariableType = SHADER_RESOURCE_VARIABLE_TYPE_STATIC;
+
+	m_pDevice->CreateGraphicsPipelineState( PSOCreateInfo, &m_pPSO );
+
+	// Since we did not explcitly specify the type for 'Constants' variable, default
+	// type (SHADER_RESOURCE_VARIABLE_TYPE_STATIC) will be used. Static variables never
+	// change and are bound directly through the pipeline state object.
+	m_pPSO->GetStaticVariableByName( SHADER_TYPE_VERTEX, "Constants" )->Set( m_VSConstants );
+
+	// Create a shader resource binding object and bind all static resources in it
+	m_pPSO->CreateShaderResourceBinding( &m_pSRB, true );
+}
+
+void HelloXrApp::CreateVertexBuffer()
+{
+	// Layout of this structure matches the one we defined in the pipeline state
+	struct Vertex
+	{
+		float3 pos;
+		float4 color;
+	};
+
+	// Cube vertices
+
+	//      (-1,+1,+1)________________(+1,+1,+1)
+	//               /|              /|
+	//              / |             / |
+	//             /  |            /  |
+	//            /   |           /   |
+	//(-1,-1,+1) /____|__________/(+1,-1,+1)
+	//           |    |__________|____|
+	//           |   /(-1,+1,-1) |    /(+1,+1,-1)
+	//           |  /            |   /
+	//           | /             |  /
+	//           |/              | /
+	//           /_______________|/
+	//        (-1,-1,-1)       (+1,-1,-1)
+	//
+
+	// clang-format off
+	Vertex CubeVerts[ 8 ] =
+	{
+		{float3( -1,-1,-1 ), float4( 1,0,0,1 )},
+		{float3( -1,+1,-1 ), float4( 0,1,0,1 )},
+		{float3( +1,+1,-1 ), float4( 0,0,1,1 )},
+		{float3( +1,-1,-1 ), float4( 1,1,1,1 )},
+
+		{float3( -1,-1,+1 ), float4( 1,1,0,1 )},
+		{float3( -1,+1,+1 ), float4( 0,1,1,1 )},
+		{float3( +1,+1,+1 ), float4( 1,0,1,1 )},
+		{float3( +1,-1,+1 ), float4( 0.2f,0.2f,0.2f,1 )},
+	};
+	// clang-format on
+
+	// Create a vertex buffer that stores cube vertices
+	BufferDesc VertBuffDesc;
+	VertBuffDesc.Name = "Cube vertex buffer";
+	VertBuffDesc.Usage = USAGE_IMMUTABLE;
+	VertBuffDesc.BindFlags = BIND_VERTEX_BUFFER;
+	VertBuffDesc.uiSizeInBytes = sizeof( CubeVerts );
+	BufferData VBData;
+	VBData.pData = CubeVerts;
+	VBData.DataSize = sizeof( CubeVerts );
+	m_pDevice->CreateBuffer( VertBuffDesc, &VBData, &m_CubeVertexBuffer );
+}
+
+void HelloXrApp::CreateIndexBuffer()
+{
+	// clang-format off
+	Uint32 Indices[] =
+	{
+		2,0,1, 2,3,0,
+		4,6,5, 4,7,6,
+		0,7,4, 0,3,7,
+		1,0,4, 1,4,5,
+		1,5,2, 5,6,2,
+		3,6,7, 3,2,6
+	};
+	// clang-format on
+
+	BufferDesc IndBuffDesc;
+	IndBuffDesc.Name = "Cube index buffer";
+	IndBuffDesc.Usage = USAGE_IMMUTABLE;
+	IndBuffDesc.BindFlags = BIND_INDEX_BUFFER;
+	IndBuffDesc.uiSizeInBytes = sizeof( Indices );
+	BufferData IBData;
+	IBData.pData = Indices;
+	IBData.DataSize = sizeof( Indices );
+	m_pDevice->CreateBuffer( IndBuffDesc, &IBData, &m_CubeIndexBuffer );
+}
+
+// Render a frame
+void HelloXrApp::Render()
+{
+	auto* pRTV = m_pSwapChain->GetCurrentBackBufferRTV();
+	auto* pDSV = m_pSwapChain->GetDepthBufferDSV();
+	// Clear the back buffer
+	const float ClearColor[] = { 0.350f, 0.350f, 0.350f, 1.0f };
+    m_pImmediateContext->SetRenderTargets( 1, &pRTV, pDSV, RESOURCE_STATE_TRANSITION_MODE_TRANSITION );
+	m_pImmediateContext->ClearRenderTarget( pRTV, ClearColor, RESOURCE_STATE_TRANSITION_MODE_TRANSITION );
+	m_pImmediateContext->ClearDepthStencil( pDSV, CLEAR_DEPTH_FLAG, 1.f, 0, RESOURCE_STATE_TRANSITION_MODE_TRANSITION );
+
+	{
+		// Map the buffer and write current world-view-projection matrix
+		MapHelper<float4x4> CBConstants( m_pImmediateContext, m_VSConstants, MAP_WRITE, MAP_FLAG_DISCARD );
+		*CBConstants = m_WorldViewProjMatrix.Transpose();
+	}
+
+	// Bind vertex and index buffers
+	Uint32   offset = 0;
+	IBuffer* pBuffs[] = { m_CubeVertexBuffer };
+	m_pImmediateContext->SetVertexBuffers( 0, 1, pBuffs, &offset, RESOURCE_STATE_TRANSITION_MODE_TRANSITION, SET_VERTEX_BUFFERS_FLAG_RESET );
+	m_pImmediateContext->SetIndexBuffer( m_CubeIndexBuffer, 0, RESOURCE_STATE_TRANSITION_MODE_TRANSITION );
+
+	// Set the pipeline state
+	m_pImmediateContext->SetPipelineState( m_pPSO );
+	// Commit shader resources. RESOURCE_STATE_TRANSITION_MODE_TRANSITION mode
+	// makes sure that resources are transitioned to required states.
+	m_pImmediateContext->CommitShaderResources( m_pSRB, RESOURCE_STATE_TRANSITION_MODE_TRANSITION );
+
+	DrawIndexedAttribs DrawAttrs;     // This is an indexed draw call
+	DrawAttrs.IndexType = VT_UINT32; // Index type
+	DrawAttrs.NumIndices = 36;
+	// Verify the state of vertex and index buffers
+	DrawAttrs.Flags = DRAW_FLAG_VERIFY_ALL;
+	m_pImmediateContext->DrawIndexed( DrawAttrs );
+}
+
+
+void HelloXrApp::Update( double CurrTime, double ElapsedTime )
+{
+	// Apply rotation
+	float4x4 CubeModelTransform = float4x4::RotationY( static_cast<float>( CurrTime ) * 1.0f ) * float4x4::RotationX( -PI_F * 0.1f );
+
+	// Camera is at (0, 0, -5) looking along the Z axis
+	float4x4 View = float4x4::Translation( 0.f, 0.0f, 5.0f );
+
+	// Get pretransform matrix that rotates the scene according the surface orientation
+    auto SrfPreTransform = float4x4::Identity(); // GetSurfacePretransformMatrix( float3 { 0, 0, 1 } );
+
+	// Get projection matrix adjusted to the current screen orientation
+    auto Proj = float4x4::Projection( 90, 16.f / 9.f, 0.01f, 10.f, false );
+
+	// Compute world-view-projection matrix
+	m_WorldViewProjMatrix = CubeModelTransform * View * SrfPreTransform * Proj;
+}
+
 
 std::unique_ptr<HelloXrApp> g_pTheApp;
 
@@ -863,7 +1039,9 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int cmdShow)
     if (!g_pTheApp->Initialize(wnd))
         return -1;
 
-    g_pTheApp->CreateResources();
+	Diligent::Timer Timer;
+
+	auto   PrevTime = Timer.GetElapsedTime();
 
     // Main message loop
     MSG msg = {0};
@@ -876,6 +1054,11 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int cmdShow)
         }
         else
         {
+			auto CurrTime = Timer.GetElapsedTime();
+			auto ElapsedTime = CurrTime - PrevTime;
+			PrevTime = CurrTime;
+            g_pTheApp->Update( CurrTime, ElapsedTime );
+
             g_pTheApp->Render();
             g_pTheApp->Present();
         }
